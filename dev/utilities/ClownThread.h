e4a000bb718770309827f6da94f35732b430be06
cfwloader <cfwloader@gmail.com>
2015-08-11 22:59:28 +0800

    Copy some codes of ClownThread

--- dev/utilities/ClownThread.h
+++ dev/utilities/ClownThread.h
@@ -1,9 +1,35 @@
 #ifndef CLOWNTHREAD_H_
 #define CLOWNTHREAD_H_
 
-namespace clown{
+#include <functional>
+
+#include "TcpServer.h"
+
+#include <pthread.h>
+
+#define MAX_LINE 2048
+
+namespace clown
+{
+	void* startThread(void*);
+
 	class Thread{
+	public:
+
+		typedef std::function<void()> ThreadFunction;
+
+		explicit Thread(const ThreadFunction&);
+
+		int start();
+
+	private:
+
+		ThreadFunction threadFunction;
+
+		pthread_t theThread;
+
 	};
 }
+
 
 #endif
