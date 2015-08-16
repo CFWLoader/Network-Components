606223bbdd97ad5dd663d2bee1dbc7d8bb270ab1
cfwloader <cfwloader@gmail.com>
2015-08-16 20:26:19 +0800

    Regular Punch-in.

--- dev/utilities/ThreadPool.h
+++ dev/utilities/ThreadPool.h
@@ -1,12 +1,20 @@
 #ifndef THREADPOOL_H_
 #define THREADPOOL_H_
 
+#include "Noncopyable.hpp"
+
 namespace clown
 {
-	class ThreadPool
+	class Thread;
+
+	class ThreadPool : public utilities::Noncopyable
 	{
 	public:
 	private:
+		
+		Thread* threads;
+
+		int numberOfThread;
 	};
 }
 
