7e52bf106339a9b22fc15c4c2a584714d5a1ba50
CFWLoader <cfwloader@gmail.com>
2016-09-01 22:08:32 +0800

    Learn how to construct Atomic classes.

--- include/util/Atomic.h
+++ include/util/Atomic.h
@@ -16,7 +16,45 @@ namespace nc
 			AtomicIntegerT() : value_(0)
 			{}
 
-			
+			T get()
+			{
+				return __sync_val_compare_and_swap(&value, 0, 0);		// GNU compiler builtin function.
+			}
+
+			T getAndAdd(T x)
+			{
+				return __sync_fetch_and_add(&value_, x);
+			}
+
+			T addAndGet(T x)
+			{
+				return getAndAdd(x) + x;
+			}
+
+			T incrementAndGet()
+			{
+				return addAndGet(1);
+			}
+
+			T decrementAndGet()
+			{
+				return addAndGet(-1);
+			}
+
+			void add(T x)
+			{
+				getAndAdd(x);
+			}
+
+			void decrement()
+			{
+				decrementAndGet();
+			}
+
+			T getAndSet(T newValue)
+			{
+				return __sync_lock_test_and_set(&value_, newValue);
+			}
 
 		private:
 
@@ -24,6 +62,9 @@ namespace nc
 
 		}
 	}
+
+	typedef util::AtomicIntegerT<int32_t> AtomicInt32;
+	typedef util::AtomicIntegerT<int64_t> AtomicInt64;
 }
 
 #endif
