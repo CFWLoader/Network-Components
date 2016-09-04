b5d909fa4f9982299de9048e9a943c75edbcbbb8
CFWLoader <cfwloader@gmail.com>
2016-09-04 17:09:33 +0800

    BlockingQueue.h

---
+++ include/util/BoundedBlockingQueue.h
@@ -0,0 +1,20 @@
+#ifndef UTIL_BOUNDEDBLOCKINGQUEUE_H_
+#define UTIL_BOUNDEDBLOCKINGQUEUE_H_
+
+#include <nc/util/Condition.h>
+#include <nc/util/Mutex.h>
+
+#include <nc/util/Noncopyable.hpp>
+
+#include <assert.h>
+
+namespace nc
+{
+	template<typename T>
+	class BoundedBlockingQueue : util::Noncopyable
+	{
+		
+	}
+}
+
+#endif
