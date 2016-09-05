c266dd34943cb3a8fa5645469438711b5571e3fa
CFWLoader <cfwloader@gmail.com>
2016-09-05 14:39:29 +0800

    Copied BoundedBlockingQueue, pause coding an auxiliary class.

--- include/util/BoundedBlockingQueue.h
+++ include/util/BoundedBlockingQueue.h
@@ -5,6 +5,7 @@
 #include <nc/util/Mutex.h>
 
 #include <nc/util/Noncopyable.hpp>
+#include <nc/util/CircularBuffer.hpp>
 
 #include <assert.h>
 
@@ -17,6 +18,73 @@ namespace nc
 
 		using namespace util;
 
+		explicit BoundedBlockingQueue(int maxSize) : mutex_(), notEmpty_(), notFull_(mutex_), queue_(maxSize)
+		{}
+
+		void pust(const T& x)
+		{
+			MutexLockGuard lock(mutex_);
+
+			while(queue_.full())
+			{
+				notFull_.wait();
+			}
+
+			assert(!queue_.full());
+
+			queue_.push_back(x);
+
+			notEmpty_.nofity();
+		}
+
+		T take()
+		{
+			MutexLockGuard lock(mutex_);
+
+			while(queue_.empty())
+			{
+				notEmpty_.wait();
+			}
+
+			assert(!queue_.empty());
+
+			T front(queue_.front());
+
+			queue_.pop_front();
+
+			notFull_.notify();
+
+			return front;
+		}
+
+		bool empty() const
+		{
+			MutexLockGuard lock(mutex_);
+
+			return queue_.empty();
+		}
+
+		bool full() const
+		{
+			MutexLockGuard lock(mutex_);
+
+			return queue_.full();
+		}
+
+		size_t size() const
+		{
+			MutexLockGuard lock(mutex_);
+
+			return queue_.size();
+		}
+
+		size_t capacity() const
+		{
+			MutexLockGuard lock(mutex_);
+
+			return queue_.size();
+		}
+
 	private:
 
 		mutable MutexLock mutex_;
@@ -25,7 +93,7 @@ namespace nc
 
 		Condition notFull_;
 
-
+		CircularBuffer<T> queue_;
 	};
 }
 
