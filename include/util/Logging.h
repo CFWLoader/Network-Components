f42b7e33852af873683911ebec36a1f6c5c50ff8
CFWLoader <cfwloader@gmail.com>
2016-09-13 09:12:11 +0800

    Logging.h, ProcessInfo.h

--- include/util/Logging.h
+++ include/util/Logging.h
@@ -68,8 +68,82 @@ namespace nc
 		{
 			return impl_.stream_;
 		}
+
+		static LogLevel logLevel();
+
+		static void setLogLevel(LogLevel level);
+
+		typedef void (*OutputFunc)(const char* msg, int len);
+		typedef void (*FlushFunc);
+
+		static void setOutput(OutputFunc);
+
+		static void setFlush(FlushFunc);
+
+		static void setTimeZone(const TimeZone& tz);
+
 	private:
+
+		class Impl
+		{
+		public:
+			typedef Logger::LogLevel LogLevel;
+
+			Impl(LogLevel level, int old_errno, const SourceFile& file, int line);
+
+			void formatTime();
+
+			void finish();
+
+			Timestamp time_;
+
+			LogStream stream_;
+
+			LogLevel level_;
+
+			int line_;
+
+			SourceFile basename_;
+		private:
+		};
+
+		Impl impl_;
 	};
+
+	extern Logger::LogLevel g_logLevel;
+
+	inline Logger::LogLevel Logger::logLevel()
+	{
+		return g_logLevel;
+	}
+
+#define LOG_TRACE if (nc::Logger::logLevel() <= nc::Logger::TRACE) \
+	nc::Logger(__FILE__, __LINE__, nc::Logger::TRACE, __func__).stream()
+#define LOG_DEBUG if (nc::Logger::logLevel() <= nc::Logger::DEBUG) \
+	nc::Logger(__FILE__, __LINE__, nc::Logger::DEBUG, __func__).stream()
+#define LOG_INFO if (nc::Logger::logLevel() <= nc::Logger::INFO) \
+	nc::Logger(__FILE__, __LINE__).stream()
+#define LOG_WARN nc::Logger(__FILE__, __LINE__, nc::Logger::WARN).stream()
+#define LOG_ERROR nc::Logger(__FILE__, __LINE__, nc::Logger::ERROR).stream()
+#define LOG_FATAL nc::Logger(__FILE__, __LINE__, nc::Logger::FATAL).stream()
+#define LOG_SYSERR nc::Logger(__FILE__, __LINE__, false).stream()
+#define LOG_SYSFATAL nc::Logger(__FILE__, __LINE__, true).stream()
+
+	const char* strerror_tl(int savedErrno);
+
+#define CHECK_NOTNULL(val) \
+	::nc::CheckNotNull(__FILE__, __LINE__, "'" #val "' Must be non NULL", (val))
+
+	template<typename T>
+	T* CheckNotNull(Logger::SourceFile file, int line, const char* names, T* ptr)
+	{
+		if (ptr == NULL)
+		{
+			Logger(file, line, Logger::FATAL).stream() << names;
+		}
+
+		return ptr;
+	}
 }
 
 #endif
