608fddfcab05800e7deb51a3e21a91d43ba10de9
CFWLoader <cfwloader@gmail.com>
2016-09-14 10:14:32 +0800

    Socket.h & SocketsOps.h

--- include/net/Socket.h
+++ include/net/Socket.h
@@ -1,19 +1,53 @@
-#ifndef _SOCKET_H_
-#define _SOCKET_H_
+#ifndef NET_SOCKET_H_
+#define NET_SOCKET_H_
 
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <nc/util/Noncopyable.hpp>
+
+struct tcp_info;
 
 namespace nc
 {	
 	namespace net
 	{
-		class Socket{
-			public:
-				Socket();
-				int getSocketFileDescriptor() const;
-			private:
-				int socketFileDescriptor;
+		class InetAddress;
+
+		class Socket : util::Noncopyable
+		{
+		public:
+
+			explicit Socket(int sockfd) : sockfd_(sockfd)
+			{}
+
+			~Socket();
+
+			int fd() const
+			{
+				return sockfd_;
+			}
+
+			bool getTcpInfo(struct tcp_info*) const;
+
+			bool getTcpInfoString(char* buf, int len) const;
+
+			void bindAddress(const InetAddress& localaddr);
+
+			void listen();
+
+			int accept(InetAddress* peeraddr);
+
+			void shutdownWrite();
+
+			void setTcpNoDelay(bool on);
+
+			void setReuseAddr(bool on);
+
+			void setReusePort(bool on);
+
+			void setKeepAlive(bool on);
+
+		private:
+
+			const int sockfd_;
 		};
 	}
 }
