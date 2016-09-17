5609353c4de4f8113ae186dd5d3efcd8b4edaee7
CFWLoader <cfwloader@gmail.com>
2016-09-17 23:24:21 +0800

    Finish copying Buffer.h

--- include/net/Buffer.h
+++ include/net/Buffer.h
@@ -232,7 +232,193 @@ namespace nc
 				append(&be64, sizeof be64);
 			}
 
+			void appendInt32(int32_t x)
+			{
+				int32_t be32= sockets::hostToNetWork32(x);
+
+				append(&be32, sizeof be32);
+			}
+
+			void appendInt16(int16_t x)
+			{
+				int16_t be16 = sockets::hostToNetWork16(x);
+
+				append(&be16, sizeof be16);
+			}
+
+			void appendInt8(int8_t x)
+			{
+				append(&x, sizeof x);
+			}
+
+			int64_t readInt64()
+			{
+				int64_t result = peekInt64();
+
+				retrieveInt64();
+
+				return result;
+			}
+
+			int32_t readInt32()
+			{
+				int32_t result = peekInt32();
+
+				retrieveInt32();
+
+				return result;
+			}
+
+			int16_t readInt16()
+			{
+				int16_t result = peekInt16();
+
+				retrieveInt16();
+
+				return result;
+			}
+
+			int8_t readInt8()
+			{
+				int8_t result = peekInt8();
+
+				retrieveInt8();
+
+				return result;
+			}
+
+			int64_t peekInt64() const
+			{
+				assert(readableBytes() >= sizeof(int64_t));
+
+				int64_t be64 = 0;
+
+				::memcpy(&be64, peek(), sizeof be64);
+
+				return sockets::networkToHost64(be64);
+			}
+
+			int32_t peekInt32() const
+			{
+				assert(readableBytes >= sizeof(int32_t));
+
+				int32_t be32 = 0;
+
+				::memcpy(&be32, peek(), sizeof be32);
+
+				return sockets::networkToHost32(be32);
+			}
+
+			int16_t peekInt16() const
+			{
+				assert(readableBytes() >= sizeof be16);
+
+				int16_t be16 = 0;
+
+				::memcpy(&be16, peek(), sizeof be16);
+
+				return sockets::networkToHost16(be16);
+			}
+
+			int8_t peekInt8() const
+			{
+				assert(readableBytes() >= sizeof(int8_t));
+
+				int8_t x = *peek();
+				
+				return x;
+			}
+
+			void prependInt64(int64_t x)
+			{
+				int64_t be64 = sockets::hostToNetWork64(x);
+
+				prepend(&be64, sizeof be64);
+			}
+
+			void prependInt32(int32_t x)
+			{
+				int32_t be32 = sockets::hostToNetWork32(x);
+
+				prepend(&be32, sizeof be32);
+			}
+
+			void prependInt16(int16_t x)
+			{
+				int16_t be16 = sockets::hostToNetWork16(x);
+
+				prepend(&be16, sizeof be16);
+			}
+
+			void prependInt8(int8_t x)
+			{
+				prepend(&x, sizeof x);
+			}
+
+			void prepend(const void* data, size_t len)
+			{
+				assert(len <= prependableBytes());
+
+				readerIndex_ -= len;
+
+				const char* d = static_cast<const char*>(data);
+
+				std::copy(d, d + len, begin() + readerIndex_);
+			}
+
+			void shrink(size_t reserve)
+			{
+				Buffer other;
+
+				other.ensureWritableBytes(readableBytes() + reserve);
+
+				other.append(toStringPiece());
+
+				swap(other);
+			}
+
+			size_t internalCapacity() const
+			{
+				return buffer_.capacity();
+			}
+
+			ssize_t readFd(int fd, int* savedErrno);
+
 		private:
+
+			char* begin()
+			{
+				return &*buffer_.begin();
+			}
+
+			const char* begin() const
+			{
+				return &*buffer_.begin();
+			}
+
+			void makeSpace(size_t len)
+			{
+				if(writableBytes() + prependableBytes() < len + kCheapPrepend)
+				{
+					buffer_.resize(writerIndex_ + len);
+				}
+				else
+				{
+					assert(kCheapPrepend < readerIndex_)
+
+					size_t readable = readableBytes();
+
+					std::copy(begin() + readableBytes(),
+						begin() + readerIndex_,
+						begin() + kCheapPrepend);
+
+					readerIndex_ = kCheapPrepend;
+
+					writerIndex_ = readerIndex_ + readable;
+
+					assert(readable == readableBytes());
+				}
+			}
 
 			std::vector<char> buffer_;
 
