df45e93a7593aabe475f37eb323a9320205fc2c9
CFWLoader <cfwloader@gmail.com>
2016-09-26 19:34:05 +0800

    Acceptor.h & BoilerPlate.h

---
+++ include/net/BoilerPlate.h
@@ -0,0 +1,18 @@
+#ifndef NET_BOILERPLATE_H_
+#define NET_BOILERPLATE_H_
+
+#include <nc/util/Noncopyable.hpp>
+
+namespace nc
+{
+	namespace net
+	{
+		class BoilerPlate
+		{
+		public:
+		private:
+		};
+	}
+}
+
+#endif
