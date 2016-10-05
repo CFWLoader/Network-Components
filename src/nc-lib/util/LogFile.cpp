a77cc5092804972ab4a16f37173c2094f0796001
CFWLoader <cfwloader@gmail.com>
2016-10-05 16:51:17 +0800

    LogFile.cpp & LogStream.cpp

--- src/nc-lib/util/LogFile.cpp
+++ src/nc-lib/util/LogFile.cpp
@@ -76,3 +76,54 @@ void logFile::append_unlocked(const char* logline, int len)
 		}
 	}
 }
+
+bool LogFile::rollFile()
+{
+	time_t now = 0;
+
+	string filename = getLogFileName(basename_, &now);
+
+	time_t start = now / kRollPerSeconds_ * kRollPerSeconds_;
+
+	if(now > lastRoll_)
+	{
+		lastRoll_ = now;
+
+		lastFlush_ = now;
+
+		startOfPeriod_ = start;
+
+		file_.reset(new FileUtil::AppendFile(filename));
+
+		return true;
+	}
+
+	return false;
+}
+
+string LogFile::getLogFileName(const string& basename, time_t* now)
+{
+	string filename;
+
+	filename.reserve(basename.size() + 64);
+
+	filename = basename;
+
+	char timebuf[32];
+
+	gmtime_r(now, &tm);
+
+	strftime(timebuf, sizeof timebuf, ".%Y%m%d-%H%M%S.", &tm);
+
+	filename += ProcessInfo::hostname();
+
+	char pidbuf[32];
+
+	snprintf(pidbuf, sizeof pidbuf, ".%d", ProcessInfo::pid());
+
+	filename += pidbuf;
+
+	filename += ".log";
+
+	return filename;
+}
