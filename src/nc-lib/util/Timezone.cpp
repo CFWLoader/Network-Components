8f4b5b042afc495f0e37126073171c55c3b51b96
CFWLoader <cfwloader@gmail.com>
2016-10-14 09:01:04 +0800

    Timezone.cpp

--- src/nc-lib/util/Timezone.cpp
+++ src/nc-lib/util/Timezone.cpp
@@ -117,8 +117,91 @@ namespace nc
 			File(const char* file) :
 				fp_(::fopen(file, "rb"))
 			{}
+
+			~File()
+			{
+				if(fp_)
+				{
+					::fclose(fp_);
+				}
+			}
+
+			bool valid() const
+			{
+				return fp_ != NULL;
+			}
+
+			string readBytes(int n)
+			{
+				char buf[n];
+
+				ssize_t nr = ::fread(buf, 1, n, fp_);
+
+				if(nr != n)
+				{
+					throw logic_error("no enough data");
+				}
+
+				return string(buf, n);
+			}
+
+			int32_t readInt32()
+			{
+				int32_t x = 0;
+
+				ssize_t nr = ::fread(&x, 1, sizeof(int32_t), fp_);
+
+				if(nr != sizeof(int32_t))
+				{
+					throw logic_error("bad int32_t data");
+				}
+
+				return be32toh(x);
+			}
+
+			uint8_t readUInt8()
+			{
+				uint8_t x = 0;
+
+				ssize_t nr = ::fread(&x, 1, sizeof(uint8_t), fp_);
+
+				if(nr != sizeof(uint8_t))
+				{
+					throw logic_error("bad uint8_t data");
+				}
+
+				return x;
+			}
 			
 		private:
+
+			FILE* fp_;
 		};
+
+		bool readTimeZoneFile(const char* zonefile, struct TimeZone::Data* data)
+		{
+			File f(zonefile);
+
+			if(f.valid())
+			{
+				try
+				{
+					string head = f.readBytes(4);
+
+					if(head != "TZif")
+					{
+						throw logic_error("bad head");
+					}
+
+					string version = f.readBytes(1);
+
+					int32_t isgmtcnt = f.readInt32();
+
+					int32_t isstdcnt = f.readInt32();
+
+					int32_t leapcnt = f.readInt32();
+				}
+			}
+		}
 	}
 }
